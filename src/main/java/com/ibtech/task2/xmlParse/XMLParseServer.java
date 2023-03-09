e03cf2b4383ec57f0dea097d9034ced431add3bc
Can <44982664+cancakmk@users.noreply.github.com>
2023-03-09 14:34:01 +0300

    Update XMLParseServer.java

--- src/main/java/com/ibtech/task2/xmlParse/XMLParseServer.java
+++ src/main/java/com/ibtech/task2/xmlParse/XMLParseServer.java
@@ -24,34 +24,17 @@ import com.ibtech.bag.BagKey;
 @WebServlet("/XMLParseTask")
 public class XMLParseServer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public XMLParseServer() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		
-		
 		XMLParse xmlParse = new XMLParse();
 		try {
 			
@@ -71,8 +54,4 @@ public class XMLParseServer extends HttpServlet {
 		}  catch (Exception e) {
 		
 			e.printStackTrace();
-		}
-		
-	}
-
-}
+		}}}
