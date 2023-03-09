ebb60b9981f82627d228113d84cd11502f47c492
Can <44982664+cancakmk@users.noreply.github.com>
2023-03-09 13:51:02 +0300

    Update AccountOperation.java

--- src/main/java/com/ibtech/command/operation/AccountOperation.java
+++ src/main/java/com/ibtech/command/operation/AccountOperation.java
@@ -3,28 +3,14 @@ package com.ibtech.command.operation;
 public class AccountOperation {
 	
 	public void Add() {
-		System.out.println("Add Çalıştı");
-		
-		
-		
-		
+		System.out.println("Add Çalıştı");	
 	}
 	
 public void Update() {
-	System.out.println("Update Çalıştı");
-		
-		
-		
-		
-		
+	System.out.println("Update Çalıştı");		
 	}
 public void Delete() {
-	System.out.println("Delete Çalıştı");
-	
-	
-	
-	
-	
+	System.out.println("Delete Çalıştı");	
 }
 
 }
